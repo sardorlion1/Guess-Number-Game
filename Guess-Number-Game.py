f0c19ed1c835495436c20d613b0faaa783ff09c4
sardorlion1 <143158519+sardorlion1@users.noreply.github.com>
2023-09-09 12:21:10 +0500

    Update Guess-Number-Game.py

--- Guess-Number-Game.py
+++ Guess-Number-Game.py
@@ -4,14 +4,14 @@ Tries = 0
 guess = 0
 
 name = input(("Hello, Welcome to the 'Guess Number Game' \nTo start the game Enter your name here : "))
-number = random.randint(1, 320)
+number = random.randint(1, 50)
 
-print("Hi, " + name + "!, I am thinking of a number between 1 and 20\nYou have six tries to find my guess number")
+print("Hi, " + name + "!, I am thinking of a number between 1 and 50.\nYou have six tries to find my guess number.")
 while Tries < 6:
     try:
         guess = int(input("Take a guess: "))  
         Tries = Tries + 1
-
+########################################################################
         if guess < number:
             if number - guess <=3:
                 print("Your guess is a little bit lower.;)")    
@@ -22,16 +22,17 @@ while Tries < 6:
             if guess - number <=3:
                 print("Your guess is a little bit higher.;)")  
             else:
-                print("Your guess is too high.")#+str(number)
+                print("Your guess is too high.")
+#In here👆 You can know the your guessed number higher or lower to a random chose number
         else:
             if guess == number:
                 break      
-    except TypeError and ValueError:
+    except TypeError and ValueError: # Here you can handle errors 
         print("Please enter valid number!:(")
 
-if guess == number:
+if guess == number:#Here if you guessed number less than 6 tries it will print 'Well done...'
     Tries = str(Tries)
-    print('Well Done, ' + name + '! You guessed my number in' + Tries + ' guesses! :-D')
-if guess != number:
+    print('Well Done, ' + name + '! You guessed my number in ' + Tries + ' guesses! :-D')
+if guess != number:# Here if you guessed number more than 6 tries it will print 'Nope...' 
     number = str(number)
     print('Nope. The number I was thinking of was ' + number)
